0ed778aedc28d2b1b49700e256123411757ee03e
Joshefs-Rubio <almn.joshefsrubio@cbtis66.edu.mx>
2024-08-13 21:39:03 -0600

    ADAE | 13 agosto

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { LogInComponent } from './log-in/log-in.component';
 
 const routes: Routes = [
+  { path: "", component: HomepageComponent},
   { path: "main", component: HomepageComponent},
   { path: "login", component: LogInComponent}
 
