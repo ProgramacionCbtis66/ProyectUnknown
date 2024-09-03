a3c8b19f482e8295cb63deeaea8915dc6675f7a2
Axolote90 <luis.manuel0456@gmail.com>
2024-09-03 15:56:16 -0600

    update app.js

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -4,15 +4,25 @@ const app = express();
 import bodyparse from 'body-parser';
 import cors from 'cors';
 
-app.use (bodyparse.urlencoded({extended: false}));
+app.use(bodyparse.urlencoded({ extended: false }));
 app.use(bodyparse.json());
 
 const cleintesPermitidos = ['http://localhost:4200'];
 
-app.use(cors({
+/* app.use(cors({
     origin: (origin, callback) => {
-        if (!origin || clientesPermitidos.indexOf(origin) !== -1) { 
+        if (!origin || clientesPermitidos.indexOf(origin) !== -1) {
             callback(null, true);
         } else {
             callback(new Error('Este dominio no esta permitido'));
-        }));
+        })); */
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'),
+        res.header('Access-Control-Allow-Headers', 'origin, X-Requestd-with, content-type,accept , aythorization');
+    if (req.method === 'OPTIONS') {
+        res.header('Acces-Control-Allow-Methods', 'GET,POST,PUT,DELETE,PATCH');
+        return rest.status(200).json({ok: 'ok'});
+    }
+    next();
+});
