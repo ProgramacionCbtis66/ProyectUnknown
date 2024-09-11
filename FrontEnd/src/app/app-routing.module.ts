7d6873962c538c0ff783b7050f32280e38c1ad71
Joshefs-Rubio <almn.joshefsrubio@cbtis66.edu.mx>
2024-09-11 10:54:25 -0600

    cambios al css del Dashboard, implementacion del componente Profile_User
    *se siguio con un poco de configuraciones del dashboard
    *se implemento el componente"Profile_User" para ver datos del usuario

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { LogInComponent } from './log-in/log-in.component';
 import { LoginQRGeneratorComponent } from './login-qr-generator/login-qr-generator.component';
 import { MainDashboardComponent } from './main-dashboard/main-dashboard.component';
+import { MyProfileUserComponent } from './my-profile-user/my-profile-user.component';
 
 const routes: Routes = [
   { path: "", component: HomepageComponent},
   { path: "main", component: HomepageComponent},
   { path: "login", component: LogInComponent},
   { path: "QRPage", component: LoginQRGeneratorComponent},
-  { path: "Main_Dashboard", component: MainDashboardComponent}
+  { path: "Main_Dashboard", component: MainDashboardComponent},
+  { path: "Profile_User", component: MyProfileUserComponent}
 ];
 
 @NgModule({
--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { LogInComponent } from './log-in/log-in.component';
 import { LoginQRGeneratorComponent } from './login-qr-generator/login-qr-generator.component';
 import { MainDashboardComponent } from './main-dashboard/main-dashboard.component';
+import { MyProfileUserComponent } from './my-profile-user/my-profile-user.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { MainDashboardComponent } from './main-dashboard/main-dashboard.componen
     LogInComponent,
     LoginQRGeneratorComponent,
     MainDashboardComponent,
+    MyProfileUserComponent,
   ],
   imports: [
     BrowserModule,
