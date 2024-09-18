2fe06c58901b44de19781f694f78ea735f057716
Joshefs-Rubio <almn.joshefsrubio@cbtis66.edu.mx>
2024-09-18 07:36:01 -0600

    correcion frontend del menu del perfil
    vuelve a ser visible y poder despegarze con normalidad en las paginas

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+// app.component.ts
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FrontEnd';
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  @HostListener('document:click', ['$event'])
+  clickOutside(event: Event) {
+    const target = event.target as HTMLElement;
+    const menu = document.querySelector('.user-icon') as HTMLElement;
+    if (menu && !menu.contains(target)) {
+      this.menuOpen = false;
+    }
+  }
 }
