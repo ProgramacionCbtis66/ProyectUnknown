d5d6369c2260f326d847a492795b0d7a0b35445d
MayrinNoguezB <159674734+MayrinNoguezB@users.noreply.github.com>
2024-09-19 10:18:43 -0600

    arreglo bug autenticacion

--- FrontEnd/src/app/service/auth.service.ts
+++ FrontEnd/src/app/service/auth.service.ts
@@ -2,34 +2,30 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { Observable } from 'rxjs';
-import { decode } from 'jwt-decode';
 import * as Notiflix from 'notiflix';
-
 
 @Injectable({
   providedIn: 'root'
 })
-
 export class AuthService {
 
-  constructor(private http: HttpClient, jwt: JwtHelperService) {   }
+  constructor(private http: HttpClient, private jwt: JwtHelperService) { }
 
-  public login(user: any):Observable<any>{
+  public login(user: any): Observable<any> {
     return this.http.post('http://localhost:4000/apiAdae/usr/login/', user);
   }
 
-  public isAuth(): boolean{
+  public isAuth(): boolean {
     const token = localStorage.getItem("adae");
-    if (token !== null && token!== "" && !this.tokeExpired() && token !== undefined){
-      if (this.jwt.isTokenExpired(token)){
-        Notiflix.Notify.Failure('Su sesion ha expirado...');
+    if (token !== null && token !== "" && token !== undefined) {
+      if (this.jwt.isTokenExpired(token)) {
+        Notiflix.Notify.failure('Su sesión ha expirado...');
         return false;
       } else {
         return true;
       }
-  } else {
-    return false;
+    } else {
+      return false;
+    }
   }
-}
-
 }
