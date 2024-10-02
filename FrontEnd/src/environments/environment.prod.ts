9cb539d010774eabfe0b5aff4c16779989eda590
Joshefs-Rubio <almn.joshefsrubio@cbtis66.edu.mx>
2024-10-02 17:21:21 -0600

    desplegando pruebas

--- FrontEnd/src/environments/environment.prod.ts
+++ FrontEnd/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
+import { HttpHeaders } from '@angular/common/http';
 export const environment = {
-  production: true
+  production: true,
+  HTTPS: 'http://localhost:4000/apiAdae',
+  autorization: {
+    headers: new HttpHeaders({ 
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${localStorage.getItem('adae')}`
+    })},
+    proyecto:"Sistema de Gestion Educativa",
+    titulo:"ADAE",
+    telefono:"2841080373"
 };
