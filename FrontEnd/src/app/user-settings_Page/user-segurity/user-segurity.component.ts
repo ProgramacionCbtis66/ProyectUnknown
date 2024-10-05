026cc04142de1348c9ab6bbfc75274ce48994fdd
Joshefs-Rubio <almn.joshefsrubio@cbtis66.edu.mx>
2024-10-05 11:08:28 -0600

    HTML & CSS | user-segurity responsive

--- FrontEnd/src/app/user-settings_Page/user-segurity/user-segurity.component.ts
+++ FrontEnd/src/app/user-settings_Page/user-segurity/user-segurity.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-user-segurity',
@@ -6,10 +6,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-segurity.component.css']
 })
 export class UserSegurityComponent implements OnInit {
+  activeMenu: string = 'perfil'; // Elemento activo por defecto
+  sidebarActive: boolean = true; // Mostrar el sidebar por defecto en modo PC
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    window.addEventListener('resize', this.handleResize.bind(this));
+    this.handleResize();
   }
 
+  ngOnDestroy(): void {
+    window.removeEventListener('resize', this.handleResize.bind(this));
+  }
+
+  setActiveMenu(menu: string): void {
+    this.activeMenu = menu;
+  }
+
+  toggleSidebar(): void {
+    if (window.innerWidth <= 768) {
+      this.sidebarActive = !this.sidebarActive; // Alternar el sidebar en móviles
+    }
+  }
+
+  handleResize(): void {
+    if (window.innerWidth > 768) {
+      this.sidebarActive = true; // Mostrar sidebar en pantallas grandes
+    } else {
+      this.sidebarActive = false; // Ocultar sidebar en pantallas pequeñas
+    }
+  }
+
+  // Detectar clics fuera del sidebar en móviles
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: MouseEvent): void {
+    const clickedElement = event.target as HTMLElement;
+    const isClickInsideSidebar = clickedElement.closest('.sidebar');
+    const isMenuBtn = clickedElement.closest('#menuBtn');
+    
+    if (!isClickInsideSidebar && !isMenuBtn && window.innerWidth <= 768) {
+      this.sidebarActive = false; // Ocultar el sidebar
+    }
+  }
 }
