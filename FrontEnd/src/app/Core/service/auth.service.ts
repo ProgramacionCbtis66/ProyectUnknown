aede9b104893635f3de58b4267956e42179f1b8e
Josh <almn.joshefsrubio@cbtis66.edu.mx>
2025-03-06 19:15:07 -0600

    Revert "arreglos iOS"
    This reverts commit 5acdc352c9f671ff0fc9877cda12c563eb117637.

--- FrontEnd/src/app/Core/service/auth.service.ts
+++ FrontEnd/src/app/Core/service/auth.service.ts
@@ -44,6 +44,7 @@ export class AuthService {
     localStorage.removeItem("adae");
     localStorage.removeItem("fotoPerfil");
     this.limpiarSesion();
+    this.toastr.info('Sesión cerrada con éxito', 'Info');
   }
 
   limpiarSesion(): void {
@@ -137,5 +138,18 @@ export class AuthService {
       let tokecode = this.jwt.decodeToken(tokedecode);
       localStorage.setItem("adae", tokecode);
     }
+  }
+
+  public tokeExpired(): boolean {
+    const tokenDecode = this.decodifica();
+    if (!tokenDecode) {
+      return true;
+    }
+    const tiempo = (tokenDecode.exp - Date.now() / 1000);
+    if (tiempo < 0) {
+      localStorage.clear();
+      return true;
+    }
+    return false;
   }
 }
